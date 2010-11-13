8ed0a06dbdca08ad8749eb7c9f1ed58d894a6100
Karlo <karlo.hh@gmx.net>
2010-11-13 11:47:52 +0100

    added a logger in front of the cache

--- EBC-Test/src/de/jebc/ebc/proof/UpcaseBoard.java
+++ EBC-Test/src/de/jebc/ebc/proof/UpcaseBoard.java
@@ -3,6 +3,7 @@ package de.jebc.ebc.proof;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.jebc.ebc.ChannelFilter;
 import de.jebc.ebc.InChannel;
 import de.jebc.ebc.impl.AbstractBoard;
 import de.jebc.ebc.parts.Cache;
@@ -23,23 +24,43 @@ public class UpcaseBoard extends AbstractBoard {
         ConvertToUpperCase upcase = new ConvertToUpperCaseImpl();
         Cache<String, String> cache = new ReadonlyCache<String, String>();
 
-        LogChannelMonitor<String, String> requestToService = new LogChannelMonitor<String, String>(log) {
-
-			@Override
-			protected String getRequest() {
-				return "Cache is requesting a value for {}";
-			}
-
-			@Override
-			protected String getResponse() {
-				return "Cache gets value {}";
-			}
-		};
+        ChannelFilter<String, String> loggerBehindCache = getBehindCacheLogger();
+        ChannelFilter<String, String> loggerFrontCache = getFrontCacheLogger();
         
         // extend the open pins to the outside
-        request = extend(cache.get());
+        connect(loggerFrontCache.out(), cache.get());
+        request = extend(loggerFrontCache.in());
 
         // plumbing the echo board
-        monitor(cache.request(), join(upcase.request(), upcase.response()), with(requestToService));
+        monitor(cache.request(), join(upcase.request(), upcase.response()), with(loggerBehindCache));
+    }
+
+    private LogChannelMonitor<String, String> getBehindCacheLogger() {
+        return new LogChannelMonitor<String, String>(log) {
+
+            @Override
+            protected String getRequest() {
+                return "Cache is requesting a value for {}";
+            }
+
+            @Override
+            protected String getResponse() {
+                return "Cache gets value {}";
+            }
+        };
+    }
+    private LogChannelMonitor<String, String> getFrontCacheLogger() {
+        return new LogChannelMonitor<String, String>(log) {
+
+            @Override
+            protected String getRequest() {
+                return "Cache is requested for a value for {}";
+            }
+
+            @Override
+            protected String getResponse() {
+                return "Cache returns value {}";
+            }
+        };
     }
 }
